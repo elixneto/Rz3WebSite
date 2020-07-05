503ff7abfc59a824f93086e08f4e563627feb06d
Élix de Paula Rezende Neto <elixneto@gmail.com>
2020-07-05 17:32:33 -0400

    Pug + configurations

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+
+gulp.task('pug', function () {
+    return gulp.src('./src/*.pug')
+        .pipe(pug({
+            doctype: 'html',
+            pretty: true
+        }))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('assets', function () {
+    return gulp.src(['./src/assets/**/*'])
+        .pipe(gulp.dest('./dist/assets'));
+});
+
+gulp.task('build', gulp.parallel('pug', 'assets'));
